426ed71430bcdb243d27dfaf516ec3b8a2a51f56
Lovleen Kaur <128346294+lovleenkaur99@users.noreply.github.com>
2023-09-26 14:00:38 -0400

    seeds done kind of

--- server/app.py
+++ server/app.py
@@ -15,4 +15,54 @@ def index():
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
-#
+# ====================================Consumers==========================================
+
+@app.post('/consumers')
+def create_consumers():
+    pass
+
+@app.patch('/consumers/<int:id>')
+def update_consumer(id):
+    pass
+
+# =====================================Cart===============================================
+
+@app.get('/carts/<int:id>')
+def get_cart_by_id(id):
+    pass
+
+@app.post('/carts')
+def create_cart():
+    pass
+
+@app.delete('/cart/<int:id>')
+def delete_cart(id):
+    pass
+
+# ========================================Product==========================================
+
+@app.get('/products')
+def get_products():
+    pass
+
+@app.get('/products/<int:id>')
+def get_product_by_id(id):
+    pass
+
+# =========================================Price===========================================
+
+@app.get('/prices')
+def get_prices():
+    pass
+
+# =========================================Supermarket======================================
+
+@app.get('/supermarkets')
+def get_supermarkets():
+    pass
+
+@app.get('/supermarkets/<int:id>')
+def get_supermarket_by_id(id):
+    pass
+
+
