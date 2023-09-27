7ba938b8e9317cb4841f7a5b60820fcb2b3a568c
Sajiya <misa7ee@gmail.com>
2023-09-27 14:45:41 -0400

    completed components folders

---
+++ client/src/components/SupermarketCard.jsx
@@ -0,0 +1,10 @@
+function SupermarketCard() {
+
+    return (
+        <div>
+            <h1>Supermarket Card</h1>
+        </div>
+    )
+}
+
+export default SupermarketCard;
---
+++ client/src/components/SupermarketList.jsx
@@ -0,0 +1,14 @@
+import SupermarketList from "./SupermarketCard"
+
+function SupermarketList() {
+
+    return (
+        <div>
+            <h1>Supermarket List</h1>
+
+            <SupermarketCard />
+        </div>
+    )
+}
+
+export default SupermarketList;
