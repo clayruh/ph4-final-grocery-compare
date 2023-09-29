c903dbb8b17692d6c1b94eca06ed4e896157bf17
Lovleen Kaur <128346294+lovleenkaur99@users.noreply.github.com>
2023-09-29 11:44:58 -0400

    idk

--- client/src/components/CompareCard.jsx
+++ client/src/components/CompareCard.jsx
@@ -1,15 +1,17 @@
 export default function CompareCard({ cartItem }) {
 
+    
+
     return (
         <>
             <img
-              src={cartItem.product.image}
-              alt="product"
-              className="product-image"
+            src={cartItem.product.image}
+            alt="product"
+            className="product-image"
             />
             <p>Product: {cartItem.product.name}</p>
             {/* how to iterate through a list to get the price? */}
-            <p>Price: {cartItem.product.prices.price}</p>
+            <p>Price: {cartItem.product.prices[0].price}</p>
         </>
     )
 }
