3173c35b629e0fa73708bc208c6e8292be1e6f75
Sajiya <misa7ee@gmail.com>
2023-10-01 23:20:02 -0400

    vscode files went missing

--- client/src/components/CompareList.jsx
+++ client/src/components/CompareList.jsx
@@ -20,26 +20,48 @@ function SearchStores() {
     ))
 
     return (
-        <>
-          <div className="compare-cart">
-            <h2>Compare Stores</h2>
-            <div className="compare-prices-container">
-              <div className="compare-prices-labels">
-                <h4>Products:</h4>
-                <h4>Trader Joe's:</h4>
-                <h4>Erewhon:</h4>
-              </div>
-              <div className="compare-prices-items">
-                {mappedCartItems}
-              </div>
-              <div className="compare-price-total">
-                Total price:
-              </div>
-            </div>
-          </div>
-        </>
+        <div>
+
+          <h2>Compare Stores</h2>
+        
+              <h4>Products:</h4>
+              <h4>Trader Joe's:</h4>
+              <h4>Erewhon:</h4>
+            
+              {mappedCartItems}
+            
+              Total price:
+
+        </div>
+         
+    
+      
       );
     }
   
        
 export default SearchStores
+
+
+
+
+// return (
+//   <>
+//     <div className="compare-cart">
+//       <h2>Compare Stores</h2>
+//       <div className="compare-prices-container">
+//         <div className="compare-prices-labels">
+//           <h4>Products:</h4>
+//           <h4>Trader Joe's:</h4>
+//           <h4>Erewhon:</h4>
+//         </div>
+//         <div className="compare-prices-items">
+//           {mappedCartItems}
+//         </div>
+//         <div className="compare-price-total">
+//           Total price:
+//         </div>
+//       </div>
+//     </div>
+//   </>
+// );
